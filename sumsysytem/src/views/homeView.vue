36d89011223f8f554ea0dde2f63dd192838408a3
陈瑾旭 <108864288+RidenShogunei@users.noreply.github.com>
2024-04-23 15:56:03 +0800

    加了app下载
    加了app下载

--- sumsysytem/src/views/homeView.vue
+++ sumsysytem/src/views/homeView.vue
@@ -6,23 +6,45 @@
     <el-container>
       <div class="aside">
         <el-aside width="250px" class="aside">
-          <el-menu :default-active="activeLang" class="el-menu-demo" mode="vertical"
-            style="height: 100%; overflow-y: auto">
-            <el-menu-item index="vue" @click="activeLang = 'vue'">Vue</el-menu-item> <!-- 这里添加 @click -->
-            <el-menu-item index="react" @click="activeLang = 'react'">React</el-menu-item> <!-- 这里添加 @click -->
+          <el-menu
+            :default-active="activeLang"
+            class="el-menu-demo"
+            mode="vertical"
+            style="height: 100%; overflow-y: auto"
+          >
+            <el-menu-item index="vue" @click="activeLang = 'vue'"
+              >Vue</el-menu-item
+            >
+            <!-- 这里添加 @click -->
+            <el-menu-item index="react" @click="activeLang = 'react'"
+              >React</el-menu-item
+            >
+            <!-- 这里添加 @click -->
           </el-menu>
         </el-aside>
       </div>
       <el-container>
         <el-main>
           <el-row :gutter="20">
-            <el-col :xs="24" :sm="12" :md="8" :lg="6" :xl="6" v-for="system in systems" :key="system.index">
-              <el-card :body-style="{ padding: '0px' }" class="box-card" @click="open(system.url)">
-                <div style="padding: 14px;">
+            <el-col
+              v-for="system in systems"
+              :key="system.index"
+            >
+              <el-card
+                :body-style="{ padding: '0px' }"
+                class="box-card"
+                @click="open(system.url)"
+              >
+                <div style="padding: 14px">
                   {{ system.name }}
                 </div>
                 <div>
                   {{ system.description }}
+                </div>
+                <div v-if="system.name === '五系统'">
+                <br>
+                <el-button type='primary' @click.stop="downloadFile" :style="{ 'margin-bottom': '2%' }">点击下载配套使用app</el-button>
+        
                 </div>
               </el-card>
             </el-col>
@@ -30,8 +52,13 @@
         </el-main>
         <el-footer>
           <div class="footer">
-            <a href="https://beian.miit.gov.cn" target="_blank" rel="noopener noreferrer" style="color:white;">{{ ICP
-              }}</a>
+            <a
+              href="https://beian.miit.gov.cn"
+              target="_blank"
+              rel="noopener noreferrer"
+              style="color: white"
+              >{{ ICP }}</a
+            >
           </div>
         </el-footer>
       </el-container>
@@ -41,6 +68,30 @@
 
 <script lang="js" setup>
 import { ref, computed } from 'vue'
+const downloadFile = async () => {
+        let filePath = 'FifthSys.apk';
+        const apiUrl = "https://chenjinxu.top:6002";
+        const mid = 'document/uploads';
+        const fullPath = `${apiUrl}/${mid}/${filePath}`;
+
+        try {
+            const response = await fetch(fullPath);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const blob = await response.blob();
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = filePath || 'download';
+            document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
+            a.click();
+            a.remove();  //afterwards we remove the element again
+        } catch (error) {
+            alert('文件下载失败');
+        }
+    };
+
 const vueSystems = [
   { index: '1', name: '一系统', url: '/system1/', description: '本系统的主要功能包括:音乐播放器,留言板,扫雷游戏,个人日志等' },
   { index: '2', name: '二系统', url: '/system2/', description: '本系统的主要功能包括:原神星铁文案查询,星铁账号查询,股票查询' },
@@ -49,7 +100,7 @@ const vueSystems = [
 ]
 
 const reactSystems = [
-  { index: '1', name: '五系统', url: '/system5/', description: '本系统的主要功能包括:文件上传下载和管理,音频录制上传下载和管理,视频录制上传下载和管理' },
+  { index: '1', name: '五系统', url: '/system5/', description: '本系统的主要功能包括:文件，音频，视频的上传，录制，下载和管理' },
   //... 更多 React 系统
 ]
 
@@ -71,12 +122,14 @@ const systems = computed(() => {
 }
 
 .title {
-  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+    "Lucida Sans", Arial, sans-serif;
   font-size: 20px;
 }
 
 .box-card {
   margin: 20px;
+  height: auto;
 }
 
 .el-header {
